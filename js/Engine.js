83d7fe127edf988ecaddf525bbc7bd7b16a0c3d6
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-10-23 13:29:47 -0400

    isPlayerDead(), height adjusted

--- js/Engine.js
+++ js/Engine.js
@@ -84,7 +84,7 @@ class Engine {
   // the burger never dies. In your exercises you will fix this method.
   isPlayerDead = () => {
     const enemy = this.enemies.find((enemy) => {
-      if (enemy.x === this.player.x && enemy.y + ENEMY_HEIGHT > GAME_HEIGHT - PLAYER_HEIGHT + 115) {
+      if (enemy.x === this.player.x && enemy.y + ENEMY_HEIGHT > GAME_HEIGHT - PLAYER_HEIGHT) {
         return true;
       }
       else {
