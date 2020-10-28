f9ff37c8d1b6f8cb5de6fd6b61638e19ee417ed0
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-10-27 21:05:11 -0400

    Updated to manipulate the speed of bonus

--- js/Bonus.js
+++ js/Bonus.js
@@ -6,7 +6,7 @@ class Candy {
         this.y = -BONUS_HEIGHT;
         this.destroyed = false;
         this.domElement = document.createElement('img');
-        this.domElement.src = './images/candy.png';
+        this.domElement.src = './images/candy_purple.png';
         this.domElement.style.position = 'absolute';
         this.domElement.style.left = `${this.x}px`;
         this.domElement.style.top = `${this.y}px`;
@@ -15,8 +15,8 @@ class Candy {
         this.speed = Math.random() / 2 + 0.25;
     }
     
-    updateCandy(timeDiff) {
-        this.y = this.y + timeDiff * this.speed;
+    updateCandy(timeDiff, speed) {
+        this.y = this.y + timeDiff * this.speed * speed;
         this.domElement.style.top = `${this.y}px`;
         
         if (this.y > GAME_HEIGHT) {
