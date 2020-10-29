ff6b271b27a4562c35f64aedc9c838a7a195702a
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-10-29 19:16:03 -0400

    Data file final update

--- js/data.js
+++ js/data.js
@@ -1,29 +1,17 @@
-// In this file we have some data that the other source files will use.
-// Most of this data is stored in constants.
-// Constants are just variables that never change. By convention,
-// We write constants with upper case letters.
-
-// The GAME_WIDTH and GAME_HEIGHT constants denote the size
-// of the game area in pixels and is used in engine-utilities.js.
 const GAME_WIDTH = 1200;
 const GAME_HEIGHT = 800;
 
-// These constants represent the width and height of an enemy in pixels
-// as well as the maximum number of enemies on screen at any given time.
 const ENEMY_WIDTH = 120;
 const ENEMY_HEIGHT = 103;
 let MAX_ENEMIES = 3;
 
-// These constants represent the player width and height.
 const PLAYER_WIDTH = 120;
 const PLAYER_HEIGHT = 155;
 
-//These constants represent the bonus (purple candies) width and height.
 const BONUS_WIDTH = 120;
 const BONUS_HEIGHT = 107;
 let MAX_CANDIES = 2;
 
-//These constants represent the bonus (red candies) width and height.
 const BONUS_WIDTH_LEVEL3 = 120;
 const BONUS_HEIGHT_LEVEL3 = 107;
 let MAX_CANDIES_LEVEL3 = 1;
