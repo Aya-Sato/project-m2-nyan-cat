0aa82b74a38a1e511d0c3c7b4a580152b60678be
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-10-29 19:21:52 -0400

    Engine file final update

--- js/Engine.js
+++ js/Engine.js
@@ -1,90 +1,100 @@
-// The engine class will only be instantiated once. It contains all the logic
-// of the game relating to the interactions between the player and the
-// enemy and also relating to how our enemies are created and evolve over time
 class Engine {
-  // The constructor has one parameter. It will refer to the DOM node that we will be adding everything to.
-  // You need to provide the DOM node when you create an instance of the class
   constructor(theRoot) {
-    // We need the DOM element every time we create a new enemy so we
-    // store a reference to it in a property of the instance.
     this.root = theRoot;
-    // We create our hamburger.
-    // Please refer to Player.js for more information about what happens when you create a player
     this.player = new Player(this.root);
-    // Initially, we have no enemies in the game. The enemies property refers to an array
-    // that contains instances of the Enemy class
     this.enemies = [];
     this.candies = [];
-    // We add the background image to the game
+    this.specialcandies = [];
+    this.count = 0;
     addBackground(this.root);
   }
 
-  // The gameLoop will run every few milliseconds. It does several things
-  //  - Updates the enemy positions
-  //  - Detects a collision between the player and any enemy
-  //  - Removes enemies that are too low from the enemies array
   gameLoop = () => {
-    // This code is to see how much time, in milliseconds, has elapsed since the last
-    // time this method was called.
-    // (new Date).getTime() evaluates to the number of milliseconds since January 1st, 1970 at midnight.
     if (this.lastFrame === undefined) {
       this.lastFrame = new Date().getTime();
     }
 
     let timeDiff = new Date().getTime() - this.lastFrame;
-
     this.lastFrame = new Date().getTime();
-    // We use the number of milliseconds since the last call to gameLoop to update the enemy positions.
-    // Furthermore, if any enemy is below the bottom of our game, its destroyed property will be set. (See Enemy.js)
+    
     this.enemies.forEach((enemy) => {
-      enemy.update(timeDiff);
-    });
-    this.candies.forEach((candy) => {
-      candy.updateCandy(timeDiff);
+      if (levelNum.innerHTML === '1') {
+        enemy.update(timeDiff, 1);
+      }
+      else if (levelNum.innerHTML === '2') {
+        enemy.update(timeDiff, 1.25);
+      }
+      else if (levelNum.innerHTML === '3') {
+        enemy.update(timeDiff, 1.5);
+      }
     });
 
-    // We remove all the destroyed enemies from the array referred to by \`this.enemies\`.
-    // We use filter to accomplish this.
-    // Remember: this.enemies only contains instances of the Enemy class.
+    this.candies.forEach((candy) => {
+      if (levelNum.innerHTML === '1' || levelNum.innerHTML === '2') {
+        candy.updateCandy(timeDiff, 1);
+      }
+      else if (levelNum.innerHTML === '3') {
+        candy.updateCandy(timeDiff, 1.25);
+      }
+    });
+    this.specialcandies.forEach((specialcandy) => {
+      specialcandy.updateSpecialCandy(timeDiff);
+    });
+
     this.enemies = this.enemies.filter((enemy) => {
       return !enemy.destroyed;
     });
     this.candies = this.candies.filter((candy) => {
       return !candy.destroyed;
     });
+    this.specialcandies = this.specialcandies.filter((specialcandy) => {
+      return !specialcandy.destroyed;
+    });
 
-    // We need to perform the addition of enemies until we have enough enemies.
     while (this.enemies.length < MAX_ENEMIES) {
-      // We find the next available spot and, using this spot, we create an enemy.
-      // We add this enemy to the enemies array
-      const spot = nextEnemyCandySpot(this.enemies, this.candies);
-      this.enemies.push(new Enemy(this.root, spot));
+      const spot = nextEnemyCandySpot(this.enemies, this.candies, this.specialcandies);
+      const newEnemy = new Enemy(this.root, spot);
+      this.enemies.push(newEnemy);
     }
+
     while (this.candies.length < MAX_CANDIES) {
-      const spot1 = nextEnemyCandySpot(this.enemies, this.candies);
+      const spot1 = nextEnemyCandySpot(this.enemies, this.candies, this.specialcandies);
       this.candies.push(new Candy(this.root, spot1));
     }
 
-    // We check if the player is dead. If he is, we alert the user
-    // and return from the method (Why is the return statement important?)
+    if (levelNum.innerHTML === '3') {
+      setTimeout(() => {
+      while (this.specialcandies.length < MAX_CANDIES_LEVEL3) {
+          const spot2 = nextEnemyCandySpot(this.enemies, this.candies, this.specialcandies);
+          this.specialcandies.push(new CandyRed(this.root, spot2));
+        }
+      }, 2000);
+    }
+
     if (this.isPlayerDead()) {
-      window.alert('Game over');
+      ghostbusters.pause();
+      document.removeEventListener('keydown', keydownHandler);
+      setTimeout(() => {
+        const gameOverText = document.querySelector('.game-over-text');
+        gameOverText.style.display = 'block';
+        const gameOverVoice = document.getElementById('gameover');
+        gameOverVoice.play();
+      }, 1000);
+      setTimeout(() => {
+        const rematchBtn = document.querySelector('.game-over-btn');
+        rematchBtn.style.display = 'block';
+        rematchBtn.addEventListener('click', function() {
+          location.reload();
+        });
+      }, 3500);
       return;
     }
-
-    if (this.isBonusPointGained()) {
-      console.log('hello');
-    }
-
-    // If the player is not dead, then we put a setTimeout to run the gameLoop in 20 milliseconds
     setTimeout(this.gameLoop, 20);
   };
 
-  // This method is not implemented correctly, which is why
-  // the burger never dies. In your exercises you will fix this method.
   isPlayerDead = () => {
     const enemy = this.enemies.find((enemy) => {
-      if (enemy.x === this.player.x && enemy.y + ENEMY_HEIGHT > GAME_HEIGHT - PLAYER_HEIGHT +10) {
+      if (enemy.x === this.player.x && enemy.y + ENEMY_HEIGHT >= GAME_HEIGHT - PLAYER_HEIGHT) {
         return true;
       }
       else {
@@ -94,15 +104,48 @@ class Engine {
     return enemy;
   };
 
-  isBonusPointGained = () => {
-    const candy = this.candies.find((candy) => {
-      if (candy.x === this.player.x && candy.y + BONUS_HEIGHT > GAME_HEIGHT - PLAYER_HEIGHT + 50) {
-        return true;
-      }
-      else {
-        return false;
-      }
-    })
-    return candy;
+  addPointLevelOne = () => {
+    this.candies.find((candy) => {
+      if (candy.x === this.player.x && candy.y + BONUS_HEIGHT > GAME_HEIGHT + 80) {
+          this.count = this.count + 10;
+        let score = document.querySelector('.score');
+        score.innerHTML = this.count;
+        candy.destroyed = true;
+        const getPointSound = document.getElementById('get-point');
+        getPointSound.play();
+      }})
+      setTimeout(this.addPointLevelOne, 20);
+    }
+  
+  addPointLevelTwo = () => {
+    this.candies.find((candy) => {
+        if (candy.x === this.player.x && candy.y + BONUS_HEIGHT > GAME_HEIGHT + 80) {
+          this.count = this.count + 25;
+          let score = document.querySelector('.score');
+          score.innerHTML = this.count;
+          candy.destroyed = true;
+          const getPointSound = document.getElementById('get-point');
+          getPointSound.play();
+        }})
+    setTimeout(this.addPointLevelTwo, 20);
+  }
+
+  addPointLevelThree = () => {
+    const getPointSound = document.getElementById('get-point');
+    this.candies.find((candy) => {
+      if (candy.x === this.player.x && candy.y + BONUS_HEIGHT > GAME_HEIGHT + 80) {
+        this.count = this.count + 50;
+        candy.destroyed = true;
+        getPointSound.play();
+      }})
+    this.specialcandies.find((specialcandy) => {
+      if (specialcandy.x === this.player.x && specialcandy.y + BONUS_HEIGHT > GAME_HEIGHT + 80) {
+        this.count = this.count + 150;
+        specialcandy.destroyed = true;
+        getPointSound.play();
+    }})
+    let score = document.querySelector('.score');
+    score.innerHTML = this.count;
+  setTimeout(this.addPointLevelThree, 20);
   }
 }
