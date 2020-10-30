e9607b38afb589cb0959eadcf2805ea24adc97b0
ayasatograph@icloud.com <ayasatography@icloud.com>
2020-10-29 20:21:44 -0400

    main file final update

--- js/main.js
+++ js/main.js
@@ -1,27 +1,155 @@
-// We create an instance of the Engine class. Looking at our index.html,
-// we see that it has a div with an id of `"app"`
 const gameEngine = new Engine(document.getElementById('app'));
+const body = document.querySelector('body');
 
-// keydownHandler is a variable that refers to a function. The function has one parameter
-// (does the parameter name matter?) which is called event. As we will see below, this function
-// will be called every time the user presses a key. The argument of the function call will be an object.
-// The object will contain information about the key press, such as which key was pressed.
+const startBtn = document.querySelector('.startbtn');
+const startMessage = document.querySelector('.start');
+const ghostbusters = document.getElementById('ghostbusters');
+const witchLaugh = document.getElementById('witch-laugh');
+const gameEnding = document.getElementById('game-ending');
+
+let levelNum = document.querySelector('.levelnum');
+levelNum.innerHTML = 1;
+let score = document.querySelector('.score');
+let purpleCandyPoint = document.querySelector('.purple-pt');
+let redCandyImage = document.querySelector('.red-candy');
+let redCandyPoint = document.querySelector('.red');
+
+const nextLevel = document.querySelector('.next-level');
+nextLevel.style.display = 'none';
+const LevelTwoBtn = document.getElementById('level2btn');
+const LevelThreeBtn = document.getElementById('level3btn');
+
+const endgameMessage = document.querySelector('.end-game');
+endgameMessage.style.display = 'none';
+endgamePoint = document.querySelector('.total-pt');
+const endgameReplayBtn = document.querySelector('.replaybtn');
+
 const keydownHandler = (event) => {
-  // event.code contains a string. The string represents which key was press. If the
-  // key is left, then we call the moveLeft method of gameEngine.player (where is this method defined?)
   if (event.code === 'ArrowLeft') {
     gameEngine.player.moveLeft();
   }
-
-  // If `event.code` is the string that represents a right arrow keypress,
-  // then move our hamburger to the right
   if (event.code === 'ArrowRight') {
     gameEngine.player.moveRight();
   }
 };
 
-// We add an event listener to document. document the ancestor of all DOM nodes in the DOM.
 document.addEventListener('keydown', keydownHandler);
 
-// We call the gameLoop method to start the game
-gameEngine.gameLoop();
+const gameStartHandler = (event) => {
+  startMessage.style.display = 'none';
+  ghostbusters.play();
+  ghostbusters.volume = 0.5;
+  gameEngine.addPointLevelOne();
+  setTimeout(() => {
+    gameEngine.gameLoop();
+    countdown(1, 30); 
+  }, 1500);
+}
+
+function fadeAudioGhostbusters() {
+  let fadeAudio = setInterval(function () {
+      if (ghostbusters.volume != 0.0) {
+          ghostbusters.volume = Math.max(0, ghostbusters.volume - 0.1);
+      }
+      else if (ghostbusters.volume === 0.0) {
+          clearInterval(fadeAudio);
+      }
+  }, 200);
+}
+
+let timer = document.getElementById('timer');
+function countdown(minutes, seconds) {
+  timer.innerHTML = minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+  const counter = setInterval(() => {
+    if (seconds > 0) {
+      seconds = seconds - 1;
+      timer.innerHTML = minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+    } 
+    else if (seconds === 0 && minutes !== 0) {
+      minutes = minutes - 1;
+      seconds = 59;
+      timer.innerHTML = minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+    }
+    if (seconds === 1 && minutes === 0 && !gameEngine.isPlayerDead()) {
+      MAX_ENEMIES = 0;
+      MAX_CANDIES = 0;
+    }
+    else if (seconds === 0 && minutes === 0 && !gameEngine.isPlayerDead()) {
+      clearInterval(counter);
+      fadeAudioGhostbusters();
+      if (levelNum.innerHTML === '1') {
+        setTimeout(() => {
+          nextLevel.style.display = 'block';
+          level3btn.style.display = 'none';
+          witchLaugh.play();
+          witchLaugh.volume = 0.4;
+        }, 4000);
+      }
+      else if (levelNum.innerHTML === '2') {
+        setTimeout(() => {
+          nextLevel.style.display = 'block';
+          level3btn.style.display = 'inline-block';
+          level2btn.style.display = 'none';
+          witchLaugh.play();
+          witchLaugh.volume = 0.4;
+        }, 4000);
+      }
+      else if (levelNum.innerHTML === '3') {
+        MAX_CANDIES_LEVEL3 = 0;
+        setTimeout(() => {
+          gameEnding.play();
+          endgameMessage.style.display = 'block';
+          endgamePoint.innerHTML = gameEngine.count;
+          endgamePoint.style.color = 'rgb(116, 1, 178)';
+          endgameReplayBtn.style.display = 'inline-block';
+          endgameReplayBtn.addEventListener('click', function() {
+          location.reload();
+          })
+        }, 4000);
+      }
+    }
+    if (gameEngine.isPlayerDead()) { 
+      clearInterval(counter); 
+    }
+  }, 1000)
+}
+
+startBtn.addEventListener('click', gameStartHandler);
+
+function startLevelTwoHandler() {
+  gameEngine.addPointLevelOne = function() {};
+  nextLevel.style.display = 'none';
+  levelNum.innerHTML = 2;
+  ghostbusters.play();
+  ghostbusters.volume = 0.5;
+  gameEngine.addPointLevelTwo();
+  purpleCandyPoint.innerHTML = 25;
+  setTimeout(() => {
+    gameEngine.gameLoop();
+    countdown(1, 30); 
+    MAX_ENEMIES = 4;
+    MAX_CANDIES = 2;
+  }, 1500);
+}
+
+LevelTwoBtn.addEventListener('click', startLevelTwoHandler);
+
+function startLevelThreeHandler() {
+  gameEngine.addPointLevelTwo = function() {};
+  nextLevel.style.display = 'none';
+  levelNum.innerHTML = 3;
+  ghostbusters.play();
+  ghostbusters.volume = 0.5;
+  gameEngine.addPointLevelThree();
+  purpleCandyPoint.innerHTML = 50;
+  redCandyImage.style.visibility = 'visible';
+  redCandyPoint.style.visibility = 'visible';
+  setTimeout(() => {
+    gameEngine.gameLoop();
+    countdown(1, 0);
+    MAX_ENEMIES = 5;
+    MAX_CANDIES = 1;
+  }, 1500);
+}
+
+LevelThreeBtn.addEventListener('click', startLevelThreeHandler);
